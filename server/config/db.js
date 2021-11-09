7e4f9462d8eac98e95e3f62768f20938b7eb8acb
grant <gmacmil3@my.centennialcollege.ca>
2021-11-09 14:38:50 -0500

    Fixed DB link

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/assignment_contacts"
+    //"URI": "mongodb://localhost/assignment_contacts"
     //"URI": "mongodb+srv://grant:Macmilg2660@mongodbserver.a0xbg.mongodb.net/assignment_contacts?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://grant:12345@grouponeserver.lk7qk.mongodb.net/survey_list?retryWrites=true&w=majority"
 }
